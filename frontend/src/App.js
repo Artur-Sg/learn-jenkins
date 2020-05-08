e4df352941c921593c98693cc07a4ecb0cf77996
Artur <artur_sgibnev@lineate.com>
2020-05-09 00:45:50 +0400

    generate fail

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          If you see this, Jenkins builded successful, Ansible works and webhook reacted for PR too!
+          If you see this, Jenkins builded successful, Ansible works and webhook reacted for PR and send statuses to GitHub too!
         </p>
       </header>
     </div>
